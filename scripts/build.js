2fd15f2e0a29e46561f6e69ca46f08ab69672e83
Simon Treiss <2010738311@stud.fh-kufstein.ac.at>
2021-04-04 22:02:21 +0200

    latest changes

--- scripts/build.js
+++ scripts/build.js
@@ -27,14 +27,6 @@ function resizeAndMoveImg(img){
     });
 }
 
-fs.copyFileSync(asset('images/soop.jpg'), target('images/soop.jpg'));
-
-fs.copyFileSync(asset('images/brrto.jpg'), target('images/brrto.jpg'));
-
-fs.copyFileSync(asset('images/fish.jpg'), target('images/fish.jpg'));
-
-fs.copyFileSync(asset('images/pezza.jpg'), target('images/pezza.jpg'));
-
 fs.copyFileSync(asset('css/style.css'), target('css/style.css'));
 
 fs.copyFileSync(asset('js/index.js'), target('js/index.js'));
