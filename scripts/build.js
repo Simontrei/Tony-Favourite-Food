f5ccebb24ff5d545c4661c3ea9ccfbc2a2bad97b
Simon Treiss <2010738311@stud.fh-kufstein.ac.at>
2021-04-04 21:08:28 +0200

    last changes

--- scripts/build.js
+++ scripts/build.js
@@ -1,19 +1,48 @@
 const fs = require('fs');
 const path = require('path');
 
+const sharp = require('sharp');
+
 function asset(filename) {
     return path.join(__dirname + "/../assets", filename);
 }
-
 function target(filename) {
     return path.join(__dirname + "/../www/assets", filename);
 }
 
-fs.copyFileSync(asset('images/soup.jpg'), target('images/soup.jpg'));
-fs.copyFileSync(asset('images/burrito.jpg'), target('images/burrito.jpg'));
-fs.copyFileSync(asset('images/fish.jpg'), target('images/fish.jpg'));
-fs.copyFileSync(asset('images/pizza.jpg'), target('images/pizza.jpg'));
-fs.copyFileSync(asset('css/style.css'), target('css/style.css'));
-fs.copyFileSync(asset('js/index.js'), target('js/index.js'));
-//fs.copyFileSync(asset('js/jquery.js'), target('js/jquery.js'));
-//fs.copyFileSync(asset('js/lodash.js'), target('js/lodash.js'));
+function resizeAndMoveImg(img){
+
+    sharp(asset(img))
+
+    .resize(null, 200)
+
+    .jpeg()
+
+    .toFile(target(img))
+
+    .catch(error => {
+
+        console.log(error);
+
+    });
+}
+
+// fs.copyFileSync(asset('images/soup.jpg'), target('images/soup.jpg'));
+
+// fs.copyFileSync(asset('images/burrito.jpg'), target('images/burrito.jpg'));
+
+// fs.copyFileSync(asset('images/fish.jpg'), target('images/fish.jpg'));
+
+// fs.copyFileSync(asset('images/pizza.jpg'), target('images/pizza.jpg'));
+
+// fs.copyFileSync(asset('css/style.css'), target('css/style.css'));
+
+// fs.copyFileSync(asset('js/index.js'), target('js/index.js'));
+
+resizeAndMoveImg('images/soop.jpg');
+
+resizeAndMoveImg('images/brrto.jpg');
+
+resizeAndMoveImg('images/fish.jpg');
+
+resizeAndMoveImg('images/pezza.jpg');
